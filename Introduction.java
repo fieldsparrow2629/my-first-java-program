3ac5802e2f3246f147c6791f7959e47bd15fe516
Erik Blom <eblom2629@greenvilleschools.us>
2017-08-25 10:02:26 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Introduction here.
+ * 
+ * @author (Erik) 
+ * @version (8/23/17)
+ */
+public class Introduction
+{
+    // instance variables - replace the example below with your own
+   
+
+    
+    public static void main(String[] args)
+    {
+        // initialise instance variables
+        System.out.println("my name is Jon Cooper");
+        System.out.println("I was born in chicago.");
+        System.out.println("i went to clemson.");
+        System.out.println("my daughter, Ashlyn, is 16.");
+        System.out.println("i like to camp and hike.");
+        
+        int year = 2017;
+        int age = 45;
+        
+        System.out.println("year - age");
+        System.out.println(year - age);
+        System.out.println("I am " + age + " years old.");
+        
+        
+    }
+    
+}
